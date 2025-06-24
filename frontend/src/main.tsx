7b3fff756915c227d196d42f453502154d3ea823
yousongsun <yousongsun@outlook.com>
2025-06-25 10:47:55 +1200

    feat: install react-router package

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
 
 const rootElement = document.getElementById("root");
-if (rootElement) {
-	createRoot(rootElement).render(
-		<StrictMode>
+if (!rootElement) throw new Error("Root element not found");
+
+createRoot(rootElement).render(
+	<StrictMode>
+		<BrowserRouter>
 			<App />
-		</StrictMode>,
-	);
-} else {
-	console.error("Root element not found");
-}
+		</BrowserRouter>
+	</StrictMode>,
+);
