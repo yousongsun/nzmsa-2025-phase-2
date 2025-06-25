a56cb8e0148f1fb004edf194b79c42f819b087e2
yousongsun <yousongsun@outlook.com>
2025-06-25 17:07:30 +1200

    feat: integrate Redux store and update main component to use Provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router";
-import "./index.css";
 import App from "./App.tsx";
+import "./index.css";
+import { store } from "./redux/store";
 
 const rootElement = document.getElementById("root");
-if (!rootElement) throw new Error("Root element not found");
+if (!rootElement) {
+	throw new Error("Root element not found");
+}
 
 createRoot(rootElement).render(
 	<StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<Provider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
 	</StrictMode>,
 );
