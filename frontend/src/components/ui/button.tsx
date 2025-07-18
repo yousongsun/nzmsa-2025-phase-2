351a38f3b549195a36ebebc82416847c9ed4b5c4
yousongsun <yousongsun@outlook.com>
2025-07-18 12:00:24 +1200

    fix: update button component to display flex layout on default variant for improved visibility

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -12,7 +12,7 @@ const buttonVariants = cva(
 				// default:
 				// 	"bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
 				default:
-					"hidden sm:flex gap-2 bg-gradient-to-r from-blue-600 to-purple-600 hover:from-blue-700 hover:to-purple-700 text-white shadow-xs hover:bg-primary/90",
+					"flex gap-2 bg-gradient-to-r from-blue-600 to-purple-600 hover:from-blue-700 hover:to-purple-700 text-white shadow-xs hover:bg-primary/90",
 				destructive:
 					"bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
 				outline:
